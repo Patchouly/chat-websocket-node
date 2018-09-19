cee33402e3b1af9c28439e0a6e996e5ceec1b98b
Pedro Cardoso <pedropaulo.cardoso@banese.com.br>
2018-09-19 17:12:36 -0300

    Adição da validação do login

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.multiroomchat = function(application, req, res){
+
+    var login = req.body;
+
+    req.assert('nickname','Apelodi deverá ser preenchdi').notEmpty();
+    req.assert('nickname','Apelido deve ter entre 3 e 30 caracteres').len(3,30);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render("index", {validacao : errors});
+        return;
+    }
+
     res.render("chat");
 }
